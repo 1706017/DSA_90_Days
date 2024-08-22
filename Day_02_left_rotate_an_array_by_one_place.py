0fa21a1178de70cb0c2cfa4ba26a1a1ecc82737c
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2024-08-22 21:26:05 +0530

    Day_02_left_rotate_an_array_by_one_place.py

---
+++ Day_02_left_rotate_an_array_by_one_place.py
@@ -0,0 +1,10 @@
+def left_rotate_by_one_place():
+    arr = [1,2,3,4,5]
+    temp = arr[0]
+    for i in range(1,len(arr)):
+        arr[i-1]=arr[i]
+    arr[len(arr)-1]=temp
+    
+    print(arr)
+
+left_rotate_by_one_place()
